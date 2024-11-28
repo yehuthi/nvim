4253ea520b898b896678046263f1932eaf3a7eaa
yehuthi <yehuthith@gmail.com>
2024-11-28 15:34:55 +0200

    improve lazy config

--- init.lua
+++ init.lua
@@ -3,4 +3,8 @@ require("config.editor").setup {}
 local lazy_spec = {}
 lazy_spec = require("config.visual").setup(lazy_spec, {})
 
-require("config.lazy").setup(lazy_spec)
+require("config.lazy").setup {
+	install = { colorscheme = { "habamax" } },
+	checker = { enabled = true },
+	spec = lazy_spec,
+}
