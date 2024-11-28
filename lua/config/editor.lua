60736abd45b27a57e57966440f8f6da5c5e01d95
yehuthi <yehuthith@gmail.com>
2024-11-28 21:47:21 +0200

    add tab width editor option

--- lua/config/editor.lua
+++ lua/config/editor.lua
@@ -1,15 +1,18 @@
+local util = require('util')
 local export = {}
 
 ---@class config
 ---@field leader? string
 ---@field column_marker? integer
 ---@field clipboard? boolean
+---@field tab_width? integer
 
 ---@type config
 local config_default = {
 	leader = '\\',
 	column_marker = 75,
 	clipboard = false,
+	tab_width = 4,
 }
 
 ---@param config config
@@ -72,6 +75,20 @@ function export.setup(config)
 	vim.opt.splitright = true
 	vim.opt.splitbelow = true
 
+	local tab_width = config.tab_width or config_default.tab_width
+	if tab_width ~= nil then
+		util.autocmd('BufEnter', {
+			desc = string.format("%d-width tabs", tab_width),
+			group = util.augroup('tab'),
+			callback = function()
+				local ft = vim.bo.filetype
+				if ft == 'yaml' then return end
+				vim.bo.tabstop = tab_width
+				vim.bo.shiftwidth = tab_width
+				vim.bo.expandtab = ft == 'haskell'
+			end,
+		})
+	end
 end
 
 return export
